9a14a4a98530e3215fb902239c084249d8fa2c78
mustfamoolan <almoq3m@gmail.com>
2025-12-31 02:16:45 +0300

    Add automatic storage link creation in AppServiceProvider as fallback

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Create storage link if it doesn't exist and symlink function is available
+        $link = public_path('storage');
+        $target = storage_path('app/public');
+
+        if (!file_exists($link) && function_exists('symlink') && !is_link($link)) {
+            // Try to create symlink if possible
+            if (!is_dir($link)) {
+                try {
+                    if (symlink($target, $link)) {
+                        // Symlink created successfully
+                    }
+                } catch (\Exception $e) {
+                    // Silently fail - user will need to create manually via SSH
+                }
+            }
+        }
     }
 }
