52bebdb72e8f358de98b92326bfadd58dd72edda
mustfamoolan <almoq3m@gmail.com>
2026-04-19 02:38:34 +0300

    Fix duplicate migrations causing server crash

---
+++ app/Console/Commands/Waseet/PushOrdersToGateway.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands\Waseet;
+
+use Illuminate\Console\Command;
+
+class PushOrdersToGateway extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:push-orders-to-gateway';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
