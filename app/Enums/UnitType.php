ca0b1e407270f303c9bb406dc28e6a33504ceef3
mustfamoolan <almoq3m@gmail.com>
2026-04-20 05:30:47 +0300

    Fix duplicate migrations causing server crash

--- app/Enums/UnitType.php
+++ app/Enums/UnitType.php
@@ -7,6 +7,7 @@ enum UnitType: string
     case WEIGHT = 'weight';
     case CARTON = 'carton';
     case SET = 'set';
+    case PIECE = 'piece';
 
     public function label(): string
     {
@@ -14,6 +15,7 @@ enum UnitType: string
             self::WEIGHT => 'وزن',
             self::CARTON => 'كارتون',
             self::SET => 'مجموعة',
+            self::PIECE => 'قطعة / مفرد',
         };
     }
 }
